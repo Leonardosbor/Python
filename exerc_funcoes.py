043eb60104094bc27990643729c15c4d06635ec1
Leonardosbor <reis8191@gmail.com>
2026-01-26 20:41:46 -0300

    exercício de Função em Python: desconto de valor

--- exerc_funcoes.py
+++ exerc_funcoes.py
@@ -1,8 +1,22 @@
-#exercício para verificar se um número é PAR ou ÍMPAR 
+#se o preço for maior que 100 → 10% de desconto
+#se o preço for menor que 100 → não tem desconto 
 
-def par_impar(numero):
-    return 'número é PAR' if numero % 2 == 0 else 'Número é ÍMPAR'
+def desconto_produto(preco_produto):
 
-resultado = par_impar(2)
+    desconto = preco_produto * 0.10
+    preco_final = preco_produto - desconto
+
+    if preco_produto >= 100:
+        return preco_final
+    else:
+        return preco_produto
+    
+resultado = desconto_produto(80)
 print(resultado)
+    
+
+
+
+    
+
 
