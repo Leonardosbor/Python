d282db45594d9012f0435fd06729b518fa8d3bd7
Leonardosbor <reis8191@gmail.com>
2026-02-03 18:31:05 -0300

    prática de exercício de função + condicional em Python

--- exercicios.py
+++ exercicios.py
@@ -1,17 +1,8 @@
-def calcular_media_notas(n1, n2,  n3):
-    
-    soma_notas = n1 + n2 + n3
-    media_notas = soma_notas / 3
-
-    if media_notas >= 7:
-        return 'APROVADO'
-    
-    if 5 <= media_notas <= 6.9:
-        return 'RECUPERAÇÃO'
-    return 'REPROVADO'
+def verificar_ruptura(lista_numeros):
+    for item in lista_numeros:
+        if item == 0:
+            return True
+    return False 
     
 
-print(calcular_media_notas(1, 1, 1))
-
-
-
+print(verificar_ruptura([10, 45, 2, 0, 15]))
