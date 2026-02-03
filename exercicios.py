5fdde3997aff7b70856883bd0ea5fb7bf021182f
Leonardosbor <reis8191@gmail.com>
2026-02-03 20:22:31 -0300

    mesmo exercicio de função com filtro mas dessa vez com list comprehension

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,5 @@
 def filtrar_pares(lista_numeros):
     
-    nova_lista = []
-
-    for item in lista_numeros:
-        if item % 2 == 0:
-            nova_lista.append(item)
-    return nova_lista
+    return [item for item in lista_numeros if item % 2 == 0]
     
 print(filtrar_pares([1, 2, 3, 4, 5, 6, 7, 8, 9]))
