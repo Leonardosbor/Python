65b862ff0374adef41e1a7e1f11ac2b4544e8cfe
Leonardosbor <reis8191@gmail.com>
2026-02-03 20:19:53 -0300

    prática de função que filtra dados em Python

--- exercicios.py
+++ exercicios.py
@@ -1,14 +1,10 @@
-def media_segura(lista_numeros):
-
-    soma = 0
-
-    if len(lista_numeros) == 0:
-        return 'Lista vazia'
+def filtrar_pares(lista_numeros):
     
+    nova_lista = []
+
     for item in lista_numeros:
-        soma += item
-
-    media = soma / len(lista_numeros)
-    return media
-
-print(media_segura([]))    
+        if item % 2 == 0:
+            nova_lista.append(item)
+    return nova_lista
+    
+print(filtrar_pares([1, 2, 3, 4, 5, 6, 7, 8, 9]))
