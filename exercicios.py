41f3c3f9a631c58148f5c0cd0bb8d22a807cf28e
Leonardosbor <reis8191@gmail.com>
2026-02-03 20:07:52 -0300

    prática de exercício de função com validação em Python

--- exercicios.py
+++ exercicios.py
@@ -1,11 +1,14 @@
-def media_lista(lista_numeros):
-    
+def media_segura(lista_numeros):
+
     soma = 0
 
+    if len(lista_numeros) == 0:
+        return 'Lista vazia'
+    
     for item in lista_numeros:
         soma += item
-    
+
     media = soma / len(lista_numeros)
     return media
 
-print(media_lista([5, 10, 15]))
+print(media_segura([]))    
